63babbd51f38efdb8796fbf59b8e931446fa764d
notradhifauzan <84311250+notradhifauzan@users.noreply.github.com>
2023-12-29 11:10:44 +0800

    implement JavaMailSender

--- src/main/java/com/orderprocessor/order_submission/service/EmailSenderService.java
+++ src/main/java/com/orderprocessor/order_submission/service/EmailSenderService.java
@@ -1,8 +1,48 @@
 package com.orderprocessor.order_submission.service;
 
+import java.io.IOException;
+
+import org.springframework.core.io.ByteArrayResource;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import com.orderprocessor.order_submission.entity.Order;
+
+import jakarta.mail.MessagingException;
+import jakarta.mail.internet.MimeMessage;
+import lombok.AllArgsConstructor;
 
 @Service
+@AllArgsConstructor
 public class EmailSenderService {
-    
+    private JavaMailSender javaMailSender;
+
+    final String MAIL_TO = "alsyathir@gmail.com";
+
+    public void sendMailWithAttachment(Order order, MultipartFile file) throws MessagingException {
+        MimeMessage mimeMessage = javaMailSender.createMimeMessage();
+        MimeMessageHelper mimeMessageHelper = new MimeMessageHelper(mimeMessage, true);
+
+        mimeMessageHelper.setFrom("not.radhifauzan@gmail.com");
+        mimeMessageHelper.setTo(MAIL_TO);
+        mimeMessageHelper.setText(order.toString());
+        mimeMessageHelper.setSubject(order.getSubject());
+
+        // Attach the file to the email
+        try {
+            ByteArrayResource byteArrayResource = new ByteArrayResource(file.getBytes());
+            mimeMessageHelper.addAttachment(file.getOriginalFilename(),byteArrayResource);
+        } catch (IOException e) {
+            e.printStackTrace();
+            throw new MessagingException("Failed to attach file to email", e);
+        }
+
+
+        javaMailSender.send(mimeMessage);
+
+        System.out.println("mail with attachment sent successfully");
+    }
 }
